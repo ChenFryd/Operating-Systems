697a3a86515c18356def1ad99e7a37afa5429557
lenanasirov <lenanasirov@gmail.com>
2023-05-21 10:48:30 +0300

    test commit

--- Assignment2/WinFormsApp1/WinFormsApp1/Form1.cs
+++ Assignment2/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -5,6 +5,12 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
+            ; ; ;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
